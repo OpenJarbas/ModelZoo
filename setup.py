ea276dd041093b3e11a7098fba16a7ead5caf20a
jarbasal <jarbasai@mailfence.com>
2021-09-01 15:48:14 +0100

    add manifest

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup
 
 setup(
     name='JarbasModelZoo',
-    version='0.2.0a2',
-    packages=['JarbasModelZoo'],
+    version='0.2.0a2post1',
+    packages=['JarbasModelZoo',
+              'JarbasModelZoo.features',
+              "JarbasModelZoo.nltk_taggers",
+              "JarbasModelZoo.nltk_chunkers"],
     url='https://github.com/OpenJarbas/ModelZoo',
     license='apache-2.0',
     author='jarbasai',
+    include_package_data=True,
     author_email='jarbasai@mailfence.com',
     description='pretrained models (down)loader'
 )
